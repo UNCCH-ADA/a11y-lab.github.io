f15517ea3b22d73a69419bd461bc7e81c2cf8e40
KaminariOS <KaminariOS@users.noreply.github.com>
2025-12-23 23:36:57 +0000

    Deploy React build UNCCH-ADA/a11y-lab@f6d6bb87c93a06f27480dca7ee5ada84992386db

--- assets/index-C3_q-aZ0.js
+++
@@ -1,1 +0,0 @@
-import{p as r,j as e}from"./index-D0WUi9XW.js";function n(){const{user:s}=r();return e.jsx("div",{children:e.jsxs("div",{children:[e.jsxs("h1",{className:"text-2xl truncate max-w-sm",children:["Hi, ",s?.full_name||s?.email," 👋"]}),e.jsx("p",{className:"text-muted-foreground",children:"Welcome back, nice to see you again!!!"})]})})}export{n as component};
